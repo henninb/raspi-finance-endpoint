ac74b59d649bc4c4d8a22fab869fda801424a5ec
Brian Henning <henninb@msn.com>
2019-11-29 19:33:50 -0600

    updates to be able to run the app

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN mkdir -p /opt/raspi_finance_endpoint/logs
 RUN mkdir -p /opt/raspi_finance_endpoint/json_in
 RUN chown -R henninb /opt/raspi_finance_endpoint/*
 
-COPY ./build/libs/raspi-finance-endpoint.jar /opt/raspi_finance_endpoint/bin/raspi-finance-endpoint.jar
+COPY ./build/libs/raspi-finance-endpoint*.jar /opt/raspi_finance_endpoint/bin/raspi-finance-endpoint.jar
 WORKDIR /opt/raspi_finance_endpoint/bin
 USER henninb
 
--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) ]; then
+  #HOST_IP=$(ipconfig getifaddr en0)
+  HOST_IP=$(hostname -I | awk '{print $1}')
+else
+  echo $OS is not yet implemented.
+  exit 1
+fi
+
 APP=raspi_finance_endpoint
 mkdir -p logs ssl json_in
 HOST_BASEDIR=$(pwd)
 GUEST_BASEDIR=/opt/${APP}
-HOST_IP=$(ipconfig getifaddr en0)
-#HOST_IP=$(hostname -I | awk '{print $1}')
-export LOGS=$BASEDIR/logs
+LOGS=$BASEDIR/logs
 touch env.secrets
-./gradlew clean build --stacktrace
+./gradlew clean build
 docker build -t ${APP} .
-docker run -it -h ${APP} --add-host hornsup:$HOST_IP -p 8080:8080 --env-file env.local -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl --rm --name ${APP} ${APP}
+docker run -it -h ${APP} --add-host hornsup:$HOST_IP -p 8081:8080 --env-file env.local -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl --rm --name ${APP} ${APP}
 
 exit 0
