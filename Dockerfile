12a299f1779f1c04b351e6034718e09139fdcdb6
Brian Henning <henninb@msn.com>
2019-12-21 18:53:10 -0600

    updates to settings

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
 FROM openjdk:8
 
-ARG APP="raspi-finance-enpoint"
-ENV APP raspi-finance-endpoint
-RUN useradd henninb
+ARG TIMEZONE="set the time zone at build time"
+ENV TIMEZONE ${TIMEZONE}
+ARG APP="set the app at build time"
+ENV APP ${APP}
+ARG USERNAME="set the username as build time"
+ENV USERNAME=${USERNAME}
+RUN useradd ${USERNAME}
 
+RUN cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 RUN mkdir -p -m 0775 /opt/${APP}/bin
 RUN mkdir -p -m 0775 /opt/${APP}/logs/archive
 RUN mkdir -p -m 0775 /opt/${APP}/ssl
 RUN mkdir -p -m 0775 /opt/${APP}/json_in
+RUN mkdir -p -m 0775 /opt/${APP}/excel_in
+RUN mkdir -p -m 0775 /opt/${APP}/config
+RUN mkdir -p -m 0775 /opt/${APP}/json_out
 ADD ./build/libs/${APP}*.jar /opt/${APP}/bin/${APP}.jar
-RUN chown -R henninb /opt/${APP}/*
+RUN chown -R ${USERNAME}:${USERNAME} /opt/${APP}/*
 
 WORKDIR /opt/${APP}/bin
-USER henninb
+USER ${USERNAME}
 
-CMD java -jar -Xmx1g /opt/${APP}/bin/${APP}.jar
+CMD java -Duser.timezone=${TIMEZONE} -jar /opt/${APP}/bin/${APP}.jar
--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <prod or local>"
+  exit 1
+fi
+ENV=$1
+
+APP=raspi-finance-endpoint
+TIMEZONE='America/Chicago'
+USERNAME=henninb
+
+if [ $ENV = "prod" ]; then
+  echo prod
+elif [ $ENV = "local" ]; then
+  echo local
+else
+  echo "Usage: $0 <prod or local>"
+  exit 2
+fi
+
 if [ \( "$OS" = "Linux Mint" \) -o \(  "$OS" = "Ubuntu" \) ]; then
   HOST_IP=$(hostname -I | awk '{print $1}')
 elif [ "$OS" = "Arch Linux" ]; then
@@ -11,14 +30,24 @@ else
   exit 1
 fi
 
-APP=raspi-finance-endpoint
 mkdir -p logs ssl json_in
 HOST_BASEDIR=$(pwd)
 GUEST_BASEDIR=/opt/${APP}
 LOGS=$BASEDIR/logs
 touch env.secrets
+
 ./gradlew clean build
-docker build -t ${APP} .
-docker run -it -h ${APP} --add-host hornsup:$HOST_IP -p 8081:8080 --env-file env.local -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl --rm --name ${APP} ${APP}
+if [ $? -ne 0 ]; then
+  echo "gradle build failed."
+  exit 1
+fi
+
+docker build -t $APP --build-arg TIMEZONE=${TIMEZONE} --build-arg APP=${APP} --build-arg USERNAME=${USERNAME} .
+if [ $? -ne 0 ]; then
+  echo "docker build failed."
+  exit 1
+fi
+
+docker run -it -h ${APP} --add-host hornsup:$HOST_IP -p 8081:8080 --env-file env.${ENV} -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl --rm --name ${APP} ${APP}
 
 exit 0
