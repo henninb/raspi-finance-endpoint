ca46eb2dab0a00299595b17f796bb6d7ed3ab5ec
Brian Henning <henninb@msn.com>
2019-12-31 17:34:26 -0600

    logging added to class

--- src/main/kotlin/finance/utils/DateTypeConverter.kt
+++ src/main/kotlin/finance/utils/DateTypeConverter.kt
@@ -8,10 +8,12 @@ import java.sql.Date
 class DateTypeConverter : AttributeConverter<Date, Long> {
 
     override fun convertToDatabaseColumn(attribute: Date): Long {
+        println("convertToDatabaseColumn - attribute=${attribute}")
         return (attribute.time / 1000)
     }
 
     override fun convertToEntityAttribute(attribute: Long): Date {
+        println("convertToEntityAttribute - attribute=${attribute}")
         return Date(attribute * 1000)
     }
 }
