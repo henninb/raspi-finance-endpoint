7c998370a7a873a9415ca8a3359d91af86f12e58
Brian Henning <henninb@msn.com>
2020-02-01 11:58:51 -0600

    build and run updates

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [ "$OSTYPE" = "linux-gnu" ]; then
+  export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+else
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+fi
+
+echo $JAVA_HOME
+
+export PATH=${JAVA_HOME}/bin:${PATH}
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <prod or local>"
