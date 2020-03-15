fefc0a4cfef29c0119825ea8a5c90e988f614b23
Brian Henning <henninb@msn.com>
2020-03-15 11:02:56 -0500

    bootrun updates; build gradle updates

--- bootrun.sh
+++ bootrun.sh
@@ -2,21 +2,25 @@
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
 #  export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
-  export JAVA_HOME=$(dirname $(dirname $(readlink -f $(readlink -f $(which javac)) || readlink -f $(which javac))))
+  JAVA_HOME=$(dirname "$(dirname "$(readlink -f "$(readlink -f "$(which javac)")" || readlink -f "$(which javac)")")")
 else
   # macos
-  export JAVA_HOME=$(/usr/libexec/java_home)
-  #export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+  JAVA_HOME=$(/usr/libexec/java_home)
+#  JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/
+  #JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
 fi
 
+export JAVA_HOME
 export PATH=${JAVA_HOME}/bin:${PATH}
 
 touch env.secrets
 touch env.console
 
 set -a
-. ./env.console
-. ./env.secrets
+# shellcheck disable=SC1091
+source env.console
+# shellcheck disable=SC1091
+source env.secrets
 set +a
 
 ./gradlew clean build bootRun
