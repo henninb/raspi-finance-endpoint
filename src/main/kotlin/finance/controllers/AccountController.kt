847b1e94d4217b647ecb16c78bfe57bfe2dbe81b
Brian Henning <henninb@msn.com>
2020-04-13 06:52:57 -0500

    updates to the endpoints to handle warnings

--- src/main/kotlin/finance/controllers/AccountController.kt
+++ src/main/kotlin/finance/controllers/AccountController.kt
@@ -1,13 +1,11 @@
 package finance.controllers
 
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import finance.domain.Account
 import finance.services.AccountService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -17,7 +15,7 @@ import java.util.*
 open class AccountController @Autowired constructor(private var accountService: AccountService) {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    @GetMapping(path = [("/account/select/active")])
+    @GetMapping(path = ["/account/select/active"])
     fun selectAllActiveAccounts(): ResponseEntity<List<Account>> {
         val accounts : List<Account> = accountService.findAllActiveAccounts()
         if( accounts.isEmpty() ) {
@@ -27,7 +25,7 @@ open class AccountController @Autowired constructor(private var accountService: 
         return ResponseEntity.ok(accounts)
     }
 
-    @GetMapping(path = [("/account/select/all")])
+    @GetMapping(path = ["/account/select/all"])
     fun selectAllAccounts(): ResponseEntity<List<Account>> {
         val accounts : List<Account> = accountService.findAllOrderByAccountNameOwner()
         if( accounts.isEmpty() ) {
@@ -37,7 +35,7 @@ open class AccountController @Autowired constructor(private var accountService: 
         return ResponseEntity.ok(accounts)
     }
 
-    @GetMapping(path = [("/account/select/{accountNameOwner}")])
+    @GetMapping(path = ["/account/select/{accountNameOwner}"])
     fun selectByAccountNameOwner(@PathVariable accountNameOwner: String): ResponseEntity<Account> {
         val accountOptional: Optional<Account> = accountService.findByAccountNameOwner(accountNameOwner)
         if( accountOptional.isPresent) {
@@ -49,7 +47,7 @@ open class AccountController @Autowired constructor(private var accountService: 
     //curl --header "Content-Type: application/json" --request POST --data '{"accountNameOwner":"test_brian", "accountType": "credit", "activeStatus": "true","moniker": "0000"' http://localhost:8080/insert_account
     //curl --header "Content-Type: application/json" --request POST --data '{"accountNameOwner":"test_brian", "accountType": "credit", "activeStatus": "true","moniker": "0000", "totals": 0.00, "totalsBalanced": 0.00, "dateClosed": 0, "dateUpdated": 0, "dateAdded": 0}' http://localhost:8080/insert_account
     //http://localhost:8080/insert_account
-    @PostMapping(path = [("/account/insert")], consumes = [("application/json")], produces = [("application/json")])
+    @PostMapping(path = ["/account/insert"])
     fun insertAccount(@RequestBody account: Account) : ResponseEntity<String> {
         accountService.insertAccount(account)
         return ResponseEntity.ok("account inserted")
@@ -57,7 +55,7 @@ open class AccountController @Autowired constructor(private var accountService: 
 
     //http://localhost:8080/delete_account/amex_brian
     //curl --header "Content-Type: application/json" --request DELETE http://localhost:8080/delete_account/test_brian
-    @DeleteMapping(path = [("/account/delete/{accountNameOwner}")])
+    @DeleteMapping(path = ["/account/delete/{accountNameOwner}"])
     fun deleteByAccountNameOwner(@PathVariable accountNameOwner: String): ResponseEntity<String> {
         val accountOptional: Optional<Account> = accountService.findByAccountNameOwner(accountNameOwner)
 
@@ -68,7 +66,7 @@ open class AccountController @Autowired constructor(private var accountService: 
         return ResponseEntity.notFound().build() //404
     }
 
-    @PatchMapping(path = [("/account/update/{accountNameOwner}")], consumes = [("application/json-patch+json")], produces = [("application/json")])
+    @PatchMapping(path = ["/account/update"])
     fun updateTransaction(@RequestBody account: Map<String, String>): ResponseEntity<String> {
         val toBePatchedTransaction = mapper.convertValue(account, Account::class.java)
         val updateStatus: Boolean = accountService.patchAccount(toBePatchedTransaction)
