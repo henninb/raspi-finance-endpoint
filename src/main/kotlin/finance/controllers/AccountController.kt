accf0d9b794f71cc78e2cdb9d8ba4c8aed9ed278
Brian Henning <henninb@msn.com>
2020-04-20 19:09:06 -0500

    updates controllers

--- src/main/kotlin/finance/controllers/AccountController.kt
+++ src/main/kotlin/finance/controllers/AccountController.kt
@@ -8,8 +8,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import java.util.*
+import javax.validation.ConstraintViolationException
 
 @CrossOrigin
 @RestController
@@ -82,11 +85,21 @@ open class AccountController @Autowired constructor(private var accountService: 
         throw EmptyAccountException("account not updated.")
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
+    @ExceptionHandler(value = [ConstraintViolationException::class, NumberFormatException::class, MethodArgumentTypeMismatchException::class, HttpMessageNotReadableException::class])
+    fun handleBadHttpRequests(throwable: Throwable): Map<String, String>? {
+        val response: MutableMap<String, String> = HashMap()
+        logger.error("Bad Request", throwable)
+        response["response"] = "BAD_REQUEST: " + throwable.javaClass.simpleName + " , message: " + throwable.message
+        return response
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = [EmptyAccountException::class])
     open fun handleHttpNotFound(throwable: Throwable): Map<String, String> {
         val response: MutableMap<String, String> = HashMap()
-        response["response"] = "NOT_FOUND: " + throwable.javaClass.simpleName
+        logger.error("not found: ", throwable)
+        response["response"] = "NOT_FOUND: " + throwable.javaClass.simpleName + " , message: " + throwable.message
         return response
     }
 
