c9b6aeab0fdcc50b6714b760cfb460b45908f596
Brian Henning <henninb@msn.com>
2020-05-25 09:08:03 -0500

    updates

--- bootrun.sh
+++ bootrun.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$OSTYPE" = "linux-gnu" ]; then
-#  export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
-  JAVA_HOME=$(dirname "$(dirname "$(readlink -f "$(readlink -f "$(which javac)")" || readlink -f "$(which javac)")")")
-else
-  # macos
-  JAVA_HOME=$(/usr/libexec/java_home)
-#  JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/
-  #JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
-fi
+#if [ "$OSTYPE" = "linux-gnu" ]; then
+##  export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+#  JAVA_HOME=$(dirname "$(dirname "$(readlink -f "$(readlink -f "$(which javac)")" || readlink -f "$(which javac)")")")
+#else
+#  # macos
+#  JAVA_HOME=$(/usr/libexec/java_home)
+##  JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/
+#  #JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+#fi
 
-export JAVA_HOME
-export PATH=${JAVA_HOME}/bin:${PATH}
+#export JAVA_HOME
+#export PATH=${JAVA_HOME}/bin:${PATH}
 
 touch env.secrets
 touch env.console
@@ -23,6 +23,7 @@ source env.console
 source env.secrets
 set +a
 
+docker run -it -h influxdb-server -p 8086:8086 --rm --name influxdb-server -d influxdb
 ./gradlew clean build bootRun
 
 exit 0
