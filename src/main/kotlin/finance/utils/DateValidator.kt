9c8e0cb7552cee0c3dedf37bf8888fcae5e720d7
Brian Henning <brian.henning@target.com>
2020-06-24 20:22:26 -0500

    fix the validators

--- src/main/kotlin/finance/utils/DateValidator.kt
+++ src/main/kotlin/finance/utils/DateValidator.kt
@@ -1,16 +1,20 @@
 package finance.utils
 
+import org.slf4j.LoggerFactory
 import java.sql.Date
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class DateValidator : ConstraintValidator<ValidDate,Date> {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     override fun initialize(constraintAnnotation: ValidDate) {
     }
 
     override fun  isValid(value : Date , context: ConstraintValidatorContext ): Boolean {
         //greater than 1/1/2000
         println("dateToBeEvaluated: $value")
+        logger.info("dateToBeEvaluated: $value")
         return value > Date(946684800)
         //return value > Date(946684800000)
     }
--- src/main/kotlin/finance/utils/TimestampValidator.kt
+++ src/main/kotlin/finance/utils/TimestampValidator.kt
@@ -1,16 +1,20 @@
 package finance.utils
 
+import org.slf4j.LoggerFactory
 import java.sql.Timestamp
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class TimestampValidator : ConstraintValidator<ValidTimestamp,Timestamp> {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     override fun initialize(constraintAnnotation: ValidTimestamp) {
     }
 
     override fun  isValid(value : Timestamp , context: ConstraintValidatorContext ): Boolean {
         //greater than 1/1/2000
         println("timestampToBeEvaluated: $value")
+        logger.info("timestampToBeEvaluated: $value")
         return value > Timestamp(946684800000)
     }
 }
