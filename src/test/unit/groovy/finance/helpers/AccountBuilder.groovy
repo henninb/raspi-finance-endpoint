2aff259bb7bb72fd32721637b1f9060adac90a6c
Brian Henning <brian.henning@target.com>
2020-06-28 11:18:45 -0500

    updates to tests

--- src/test/unit/groovy/finance/helpers/AccountBuilder.groovy
+++ src/test/unit/groovy/finance/helpers/AccountBuilder.groovy
@@ -11,12 +11,11 @@ class AccountBuilder {
     AccountType accountType = AccountType.Credit
     Boolean activeStatus = true
     String moniker = "1234"
-    //BigDecimal totals: Double = new BigDecimal("0.0"),
-    //BigDecimal totalsBalanced: BigDecimal = 0.0,
+    BigDecimal totals = new BigDecimal("0.0")
+    BigDecimal totalsBalanced = new BigDecimal("0.0")
     Timestamp dateClosed = new Timestamp(0)
     Timestamp dateUpdated = new Timestamp(1553645394000)
     Timestamp dateAdded = new Timestamp(1553645394000)
-
 
     static AccountBuilder builder() {
         return new AccountBuilder()
@@ -28,6 +27,8 @@ class AccountBuilder {
         account.accountType = accountType
         account.activeStatus = activeStatus
         account.moniker = moniker
+        account.totals = totals
+        account.totalsBalanced = totalsBalanced
         account.dateClosed = dateClosed
         account.dateAdded = dateAdded
         account.dateUpdated = dateUpdated
@@ -43,6 +44,7 @@ class AccountBuilder {
         this.accountType = accountType
         return this
     }
+
     AccountBuilder activeStatus(activeStatus) {
         this.activeStatus = activeStatus
         return this
@@ -65,6 +67,16 @@ class AccountBuilder {
 
     AccountBuilder dateAdded(dateAdded) {
         this.dateAdded = dateAdded
+        return this
+    }
+
+    AccountBuilder totals(totals) {
+        this.totals = totals
+        return this
+    }
+
+    AccountBuilder totalsBalanced(totalsBalanced) {
+        this.totalsBalanced = totalsBalanced
         return this
     }
 }
