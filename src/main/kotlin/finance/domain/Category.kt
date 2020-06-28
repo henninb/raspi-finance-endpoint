68a63dd880f5a1b607014b554d34a103b01552b4
Brian Henning <brian.henning@target.com>
2020-06-28 06:01:21 -0500

    updates domain objects

--- src/main/kotlin/finance/domain/Category.kt
+++ src/main/kotlin/finance/domain/Category.kt
@@ -8,33 +8,24 @@ import javax.validation.constraints.Min
 import javax.validation.constraints.Size
 import org.hibernate.annotations.Proxy
 
+
 @Entity(name = "CategoryEntity")
 @Proxy(lazy = false)
 @Table(name = "t_category")
-class Category constructor(_categoryId: Long = 0L, _category: String = "") {
+data class Category(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @field:Min(value = 0L)
+        @JsonProperty
+        var categoryId: Long,
 
+        @field:Size(min = 1, max = 50)
+        @Column(unique = true)
+        @JsonProperty
+        var category: String
+
+) {
     constructor() : this(0L, "")
-
-    //TODO: add activeStatus field
-    //TODO: add dateUpdated field
-    //TODO: add dateAdded field
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Min(value = 0L)
-    @JsonProperty
-    var categoryId = _categoryId
-
-    @Size(min = 1, max = 50)
-    @Column(unique = true)
-    @JsonProperty
-    var category = _category
-
-//    @JsonProperty
-//    var dateUpdated = _dateUpdated
-//
-//    @JsonProperty
-//    var dateAdded = _dateAdded
 
     override fun toString(): String = mapper.writeValueAsString(this)
 
@@ -43,3 +34,46 @@ class Category constructor(_categoryId: Long = 0L, _category: String = "") {
         private val mapper = ObjectMapper()
     }
 }
+
+//TODO: add active_status field
+//    @JsonProperty
+//    var dateUpdated
+//
+//    @JsonProperty
+//    var dateAdded
+
+//@Entity(name = "CategoryEntity")
+//@Proxy(lazy = false)
+//@Table(name = "t_category")
+//class Category constructor(_categoryId: Long = 0L, _category: String = "") {
+//
+//    constructor() : this(0L, "")
+//
+//    //TODO: add activeStatus field
+//    //TODO: add dateUpdated field
+//    //TODO: add dateAdded field
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Min(value = 0L)
+//    @JsonProperty
+//    var categoryId = _categoryId
+//
+//    @Size(min = 1, max = 50)
+//    @Column(unique = true)
+//    @JsonProperty
+//    var category = _category
+//
+////    @JsonProperty
+////    var dateUpdated = _dateUpdated
+////
+////    @JsonProperty
+////    var dateAdded = _dateAdded
+//
+//    override fun toString(): String = mapper.writeValueAsString(this)
+//
+//    companion object {
+//        @JsonIgnore
+//        private val mapper = ObjectMapper()
+//    }
+//}
