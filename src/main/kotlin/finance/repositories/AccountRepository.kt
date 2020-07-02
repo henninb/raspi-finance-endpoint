40bd6469115cd43a55e54010ccfd1682a498ca58
Brian Henning <brian.henning@target.com>
2020-07-01 20:43:45 -0500

    fixed the bug with calculating the amount

--- src/main/kotlin/finance/repositories/AccountRepository.kt
+++ src/main/kotlin/finance/repositories/AccountRepository.kt
@@ -1,6 +1,7 @@
 package finance.repositories
 
 import finance.domain.Account
+import finance.domain.AccountType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -8,22 +9,19 @@ import java.util.*
 import javax.transaction.Transactional
 
 interface AccountRepository : JpaRepository<Account, Long> {
-    //fun findByAccountId(transactionId: Long?): Account
     fun findByAccountNameOwner(accountNameOwner: String): Optional<Account>
     fun findByActiveStatusOrderByAccountNameOwner(activeStatus: Boolean): List<Account>
-    fun findAllByOrderByAccountNameOwner(): List<Account>
+    //fun findByAccountType(accountType: AccountType): List<Account>
 
     @Modifying
     @Transactional
     @Query(value = "DELETE from t_account WHERE account_name_owner = ?1", nativeQuery = true)
     fun deleteByAccountNameOwner(accountNameOwner: String)
 
-
     @Modifying
     @Transactional
     @Query(value = "UPDATE t_account SET totals = x.totals FROM (SELECT account_name_owner, SUM(amount) AS totals FROM t_transaction GROUP BY account_name_owner) x WHERE t_account.account_name_owner = x.account_name_owner", nativeQuery = true)
     fun updateAccountClearedTotals()
-
 
     @Modifying
     @Transactional
