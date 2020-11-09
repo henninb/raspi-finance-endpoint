5b2523a2824f5e30b75a70aeee0b378b2b03fa16
Brian Henning <henninb@msn.com>
2020-11-09 06:18:04 -0600

    added tests

--- src/test/functional/groovy/finance/routes/JsonFileReaderRouteBuilderSpec.groovy
+++ src/test/functional/groovy/finance/routes/JsonFileReaderRouteBuilderSpec.groovy
@@ -1,12 +1,12 @@
 package finance.routes
 
 import finance.Application
+import finance.configurations.CamelProperties
 import finance.helpers.TransactionBuilder
 import org.apache.camel.Exchange
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import spock.lang.Ignore
 import spock.lang.Specification
 
 @ActiveProfiles("func")
@@ -16,15 +16,40 @@ class JsonFileReaderRouteBuilderSpec extends Specification {
     @Autowired
     JsonFileReaderRouteBuilder jsonFileReaderRouteBuilder
 
-    @Ignore
+    @Autowired
+    CamelProperties camelProperties
+
     def "test -- jsonFileReaderRouteBuilder"() {
         given:
         def camelContext = jsonFileReaderRouteBuilder.getContext()
         def producer = camelContext.createProducerTemplate()
-        def transactions = [TransactionBuilder.builder().build()]
+        camelContext.start()
+        //camelContext.routes.forEach(route -> route.setAutoStartup(true))
+        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
+        def transaction = TransactionBuilder.builder().build()
+        transaction.amount = 0.00
+        transaction.guid = UUID.randomUUID()
+        def transactions = [transaction]
 
         when:
         producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, 'foo_brian.json')
+
+        then:
+        1 == 1
+    }
+
+    def "test -- jsonFileReaderRouteBuilder - bad filename"() {
+        given:
+        def camelContext = jsonFileReaderRouteBuilder.getContext()
+        def producer = camelContext.createProducerTemplate()
+        camelContext.start()
+        //camelContext.routes.forEach(route -> route.setAutoStartup(true))
+        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
+        def transaction = TransactionBuilder.builder().build()
+        def transactions = [transaction]
+
+        when:
+        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, 'foo_brian.txt')
 
         then:
         1 == 1
