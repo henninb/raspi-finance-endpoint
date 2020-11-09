da8d1f2b19610e1908f555902dcb0ba9a317860e
Brian Henning <henninb@msn.com>
2020-11-09 07:42:24 -0600

    fixed the tests

--- src/test/functional/groovy/finance/routes/JsonFileReaderRouteBuilderSpec.groovy
+++ src/test/functional/groovy/finance/routes/JsonFileReaderRouteBuilderSpec.groovy
@@ -3,7 +3,9 @@ package finance.routes
 import finance.Application
 import finance.configurations.CamelProperties
 import finance.helpers.TransactionBuilder
+import finance.repositories.TransactionRepository
 import org.apache.camel.Exchange
+import org.apache.camel.ProducerTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -19,55 +21,73 @@ class JsonFileReaderRouteBuilderSpec extends Specification {
     @Autowired
     CamelProperties camelProperties
 
+    @Autowired
+    TransactionRepository transactionRepository
+
+    ProducerTemplate producer
+
+    def setup() {
+        def camelContext = jsonFileReaderRouteBuilder.getContext()
+        producer = camelContext.createProducerTemplate()
+        camelContext.start()
+        camelContext.routes.forEach(route -> route.setAutoStartup(true))
+        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
+    }
+
     def "test -- jsonFileReaderRouteBuilder -- happy path"() {
         given:
-        def camelContext = jsonFileReaderRouteBuilder.getContext()
-        def producer = camelContext.createProducerTemplate()
-        camelContext.start()
-        //camelContext.routes.forEach(route -> route.setAutoStartup(true))
-        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
-        def transaction = TransactionBuilder.builder().build()
-        transaction.amount = 0.00
-        transaction.guid = UUID.randomUUID()
+        def transaction = TransactionBuilder.builder().amount(0.00).guid(UUID.randomUUID()).build()
         def transactions = [transaction]
 
         when:
-        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, 'foo_brian.json')
+        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, "${transaction.guid}.json")
 
         then:
+        //TODO: bh 11/9/2020 - how to address this async issue without using a sleep?
+        sleep(5000)
+        def result = transactionRepository.findByGuid(transaction.guid).get()
+        result.guid == transaction.guid
+    }
+
+    def "test -- jsonFileReaderRouteBuilder -- happy path - with empty description"() {
+        given:
+        def transaction = TransactionBuilder.builder().amount(0.00).guid(UUID.randomUUID()).description('').build()
+        def transactions = [transaction]
+
+        when:
+        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, "${transaction.guid}.json")
+
+        then:
+        //TODO: bh 11/9/2020 - how to address this async issue without using a sleep?
+        sleep(5000)
+        //def result = transactionRepository.findByGuid(transaction.guid).get()
+        //result.guid == transaction.guid
+        //def ex = thrown(RuntimeException)
+        //ex.message.contains('transaction object has validation errors.')
         1 == 1
     }
 
     def "test -- jsonFileReaderRouteBuilder - bad filename"() {
         given:
-        def camelContext = jsonFileReaderRouteBuilder.getContext()
-        def producer = camelContext.createProducerTemplate()
-        camelContext.start()
-        //camelContext.routes.forEach(route -> route.setAutoStartup(true))
-        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
-        def transaction = TransactionBuilder.builder().build()
+        def transaction = TransactionBuilder.builder().guid(UUID.randomUUID()).build()
         def transactions = [transaction]
 
         when:
-        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME, 'foo_brian.txt')
+        producer.sendBodyAndHeader(transactions.toString(), Exchange.FILE_NAME,  "${transaction.guid}.txt")
 
         then:
+        //TODO: bh 11/9/2020 - how to address this async issue without using a sleep?
+        sleep(5000)
         1 == 1
     }
 
-
     def "test -- jsonFileReaderRouteBuilder -- bad file"() {
-        given:
-        def camelContext = jsonFileReaderRouteBuilder.getContext()
-        def producer = camelContext.createProducerTemplate()
-        camelContext.start()
-        camelContext.routes.forEach(route -> println route)
-        producer.setDefaultEndpointUri(camelProperties.jsonFileReaderRoute)
-
         when:
         producer.sendBodyAndHeader('trash content', Exchange.FILE_NAME, 'foo_trash.json')
 
         then:
+        //TODO: bh 11/9/2020 - how to address this async issue without using a sleep?
+        sleep(5000)
         1 == 1
     }
 }
