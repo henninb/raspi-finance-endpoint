cf83ee2844423678b4e3d2ae3383ffcbf213389e
Brian Henning <henninb@msn.com>
2020-12-01 07:37:57 -0600

    added code to investigate

--- src/main/kotlin/finance/configurations/BeanConfig.kt
+++ src/main/kotlin/finance/configurations/BeanConfig.kt
@@ -6,6 +6,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+//import org.springframework.context.embedded
 
 @Configuration
 open class BeanConfig {
@@ -21,4 +22,21 @@ open class BeanConfig {
     open fun timedAspect(registry: MeterRegistry): TimedAspect {
         return TimedAspect(registry)
     }
+
+
+//    @Bean
+//    open fun containerCustomizer() :  EmbeddedServletContainerCustomizer {
+//        val container
+//        return (container -> {
+//            container.setPort(8080);
+//        })
+//    }
+
+    //TODO: fix this bug
+    //NioEndpoint - Error running socket processor, java.lang.NullPointerException
+    //WORKAROUND: Disable TLSv1.3 and running with TLSv1.2 only. Or use OpenSSL for the encryption.
+    //I simply changed the connector protocol from org.apache.coyote.http11.Http11Protocol to org.apache.coyote.http11.Http11NioProtocol
+
+
+
 }
