c41a9e5e8365e5c1c8c22c26a1d1bac716ceb73a
Brian Henning <henninb@msn.com>
2020-12-19 19:25:04 -0600

    oracle updates

--- src/test/oracle/resources/db/migration/V02__create-all-database-objects.sql
+++ src/test/oracle/resources/db/migration/V02__create-all-database-objects.sql
@@ -1,3 +1,12 @@
+-- drop table T_TRANSACTION_CATEGORIES cascade constraints;
+-- drop table T_PAYMENT cascade constraints;
+-- drop table T_TRANSACTION cascade constraints;
+-- drop table T_ACCOUNT cascade constraints;
+-- drop table T_CATEGORY cascade constraints;
+-- drop table T_RECEIPT_IMAGE cascade constraints;
+-- drop table T_PARM cascade constraints;
+-- drop table T_DESCRIPTION cascade constraints;
+
 --------------
 -- Account --
 --------------
@@ -149,3 +158,66 @@ CREATE TABLE t_description
     date_added     TIMESTAMP          NULL
     -- CONSTRAINT t_description_description_lowercase_ck CHECK (description = lower(description))
 );
+
+-- t_account
+CREATE OR REPLACE TRIGGER tr_insert_account
+AFTER INSERT
+      ON t_account
+          FOR EACH ROW
+BEGIN
+UPDATE t_account(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_transaction_categories
+CREATE OR REPLACE TRIGGER tr_insert_transaction_categories
+AFTER INSERT
+      ON t_transaction_categories
+          FOR EACH ROW
+BEGIN
+UPDATE t_transaction_categories(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_receipt_image
+CREATE OR REPLACE TRIGGER tr_insert_receipt_image
+AFTER INSERT
+      ON t_receipt_image
+          FOR EACH ROW
+BEGIN
+UPDATE t_receipt_image(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_transaction
+CREATE OR REPLACE TRIGGER tr_insert_transaction
+AFTER INSERT
+      ON t_transaction
+          FOR EACH ROW
+BEGIN
+UPDATE t_transaction(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_payment
+CREATE OR REPLACE TRIGGER tr_insert_payment
+AFTER INSERT
+      ON t_payment
+          FOR EACH ROW
+BEGIN
+UPDATE t_payment(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_parm
+CREATE OR REPLACE TRIGGER tr_insert_parm
+AFTER INSERT
+      ON t_parm
+          FOR EACH ROW
+BEGIN
+UPDATE t_parm(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
+
+-- t_description
+CREATE OR REPLACE TRIGGER tr_insert_description
+AFTER INSERT
+      ON t_description
+          FOR EACH ROW
+BEGIN
+UPDATE t_description(date_updated, date_added) VALUES(sysdate, sysdate);
+END;
