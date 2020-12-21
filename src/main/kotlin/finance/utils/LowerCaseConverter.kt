ea172839840ff288d59102895313315a6e6109c9
Brian Henning <henninb@msn.com>
2020-12-21 08:15:09 -0600

    oracle updates

--- src/main/kotlin/finance/utils/LowerCaseConverter.kt
+++ src/main/kotlin/finance/utils/LowerCaseConverter.kt
@@ -1,5 +1,6 @@
 package finance.utils
 
+import org.apache.logging.log4j.LogManager
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
@@ -7,10 +8,24 @@ import javax.persistence.Converter
 class LowerCaseConverter : AttributeConverter<String, String> {
 
     override fun convertToDatabaseColumn(attribute: String): String {
-        return attribute.toLowerCase()
+        return try {
+            attribute.toLowerCase()
+        } catch( e: NullPointerException) {
+            logger.info(e.message)
+            ""
+        }
     }
 
     override fun convertToEntityAttribute(attribute: String): String {
-        return attribute.toLowerCase()
+        return try {
+            attribute.toLowerCase()
+        } catch( e: NullPointerException) {
+            logger.info(e.message)
+            ""
+        }
+    }
+
+    companion object {
+        private val logger = LogManager.getLogger()
     }
 }
