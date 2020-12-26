bf5478dcbdfb87959a7e9b8a806f4d4229ecc36c
Brian Henning <henninb@msn.com>
2020-12-26 06:09:52 -0600

    updates to add owing accounts

--- src/main/kotlin/finance/repositories/AccountRepository.kt
+++ src/main/kotlin/finance/repositories/AccountRepository.kt
@@ -9,7 +9,7 @@ import javax.transaction.Transactional
 
 interface AccountRepository : JpaRepository<Account, Long> {
     fun findByAccountNameOwner(accountNameOwner: String): Optional<Account>
-    fun findByActiveStatusOrderByAccountNameOwner(activeStatus: Boolean): List<Account>
+    fun findByActiveStatusOrderByAccountNameOwner(activeStatus: Boolean = true): List<Account>
 
     @Transactional
     fun deleteByAccountNameOwner(accountNameOwner: String)
@@ -32,4 +32,5 @@ interface AccountRepository : JpaRepository<Account, Long> {
 
     @Query(value = "SELECT account_name_owner FROM t_transaction WHERE transaction_state = 'cleared' and account_name_owner in (select account_name_owner from t_account where account_type = 'credit' and active_status = true) or (transaction_state = 'outstanding' and account_type = 'credit' and description ='payment') group by account_name_owner having sum(amount) > 0 order by account_name_owner", nativeQuery = true)
     fun findAccountsThatRequirePayment(): List<String>
+
 }
--- src/test/unit/groovy/finance/services/TransactionServiceSpec.groovy
+++ src/test/unit/groovy/finance/services/TransactionServiceSpec.groovy
@@ -16,6 +16,7 @@ import org.hibernate.NonUniqueResultException
 import spock.lang.Specification
 
 import javax.validation.Validator
+import java.sql.Date
 
 class TransactionServiceSpec extends Specification {
     protected TransactionRepository mockTransactionRepository = GroovyMock(TransactionRepository)
@@ -314,6 +315,38 @@ class TransactionServiceSpec extends Specification {
         transactions.size() == 1
         1 * mockTransactionRepository.findByGuid(transaction.guid) >> Optional.of(transaction)
         1 * mockTransactionRepository.saveAndFlush(transaction) >> transaction
+        0 * _
+    }
+
+    void 'test - findAccountsThatRequirePayment'() {
+        given:
+        Date today = new Date(Calendar.instance.time.time)
+        Calendar calendar = Calendar.instance
+        calendar.add(Calendar.DAY_OF_MONTH, 15)
+        Date todayPlusFifteen = new Date(calendar.time.time)
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+        Date todayPlusSixteen = new Date(calendar.time.time)
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+        Date todayPlusSeventeen = new Date(calendar.time.time)
+        calendar.add(Calendar.DAY_OF_MONTH, 35)
+        Date todayPlusPastThirty = new Date(calendar.time.time)
+        Account account1 = new Account(accountNameOwner: 'test1', accountType: AccountType.Credit)
+        Account account2 = new Account(accountNameOwner: 'test2', accountType: AccountType.Credit, totals: new BigDecimal(2), totalsBalanced: new BigDecimal(2) )
+        Account account3 = new Account(accountNameOwner: 'test3', accountType: AccountType.Credit, totalsBalanced: new BigDecimal(5))
+        Transaction transaction1 = new Transaction(accountNameOwner: 'test1', transactionState: TransactionState.Future, transactionDate: todayPlusPastThirty, amount: new BigDecimal(2.01))
+        Transaction transaction2 = new Transaction(accountNameOwner: 'test2', transactionState: TransactionState.Future, transactionDate: todayPlusFifteen, amount: new BigDecimal(2.02))
+        Transaction transaction3 = new Transaction(accountNameOwner: 'test1', transactionState: TransactionState.Outstanding, transactionDate: todayPlusPastThirty , amount: new BigDecimal(4.03))
+        Transaction transaction4 = new Transaction(accountNameOwner: 'test3', transactionState: TransactionState.Future, transactionDate: todayPlusSeventeen, amount: new BigDecimal(3.04))
+        Transaction transaction5 = new Transaction(accountNameOwner: 'test2', transactionState: TransactionState.Future, transactionDate: todayPlusSixteen, amount: new BigDecimal(2.05))
+        Transaction transaction6 = new Transaction(accountNameOwner: 'test1', amount: new BigDecimal(2.05))
+
+        when:
+        List<Account> accounts = transactionService.findAccountsThatRequirePayment()
+
+        then:
+        accounts.size() == 1
+        1 * mockAccountRepository.findByActiveStatusOrderByAccountNameOwner(true) >> [account1, account2, account3]
+        1 * mockTransactionRepository.findByAccountNameOwnerAndActiveStatusOrderByTransactionDateDesc('test3', true) >> [transaction1, transaction2, transaction3,transaction4, transaction5,transaction6]
         0 * _
     }
 }
