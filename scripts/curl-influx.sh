85a7bbe7c8dcadb14267de448d16ea9735f82fee
Brian Henning <henninb@msn.com>
2021-01-21 17:25:16 -0600

    updates

--- scripts/curl-influx.sh
+++ scripts/curl-influx.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# curl -i -X POST 'http://localhost:8086/query' -u "henninb:monday1" --data-urlencode "q=CREATE DATABASE metrics"
+server=192.168.100.124
+# curl -i -X POST "http://${server}:8086/query" -u "henninb:monday1" --data-urlencode "q=CREATE DATABASE metrics"
 
-curl -s -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value FROM stuff" > metrics-values.json
 
-curl -s -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW SERIES ON metrics" > metrics-series.json
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value FROM stuff" > stuff-values.json
 
-curl -s -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW measurements on metrics" > metrics-measurements.json
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW SERIES ON metrics" > metrics-series.json
+
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value from metrics" > metrics-values.json
+
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW measurements on metrics" > metrics-measurements.json
+
+exit 0
