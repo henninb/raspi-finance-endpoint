dcb544614977965bf08591c6122310fcf8f8cc8e
Brian Henning <henninb@msn.com>
2021-01-24 16:49:17 -0600

    updates

--- src/test/integration/groovy/finance/CamelSpec.groovy
+++ src/test/integration/groovy/finance/CamelSpec.groovy
@@ -19,12 +19,11 @@ class CamelSpec extends  Specification {
 
     protected String baseName = new FileSystemResource("").file.absolutePath
 
-    protected PollingConditions conditions = new PollingConditions(timeout: 30, initialDelay: 1.5, factor: 1.25)
+    protected PollingConditions conditions = new PollingConditions(timeout: 20, initialDelay: 1.5, factor: 1.25)
 
     @Autowired
     protected TransactionRepository transactionRepository
 
-    @Ignore
     void 'test camel send data'() {
         given:
         File source = new File(ResourceUtils.getResource('/camel-input.json').file)
