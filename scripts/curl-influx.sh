3869396611f78243bd07f3462bc562a0bc3eed77
Brian Henning <henninb@msn.com>
2021-01-23 19:38:39 -0600

    meter structural updates

--- scripts/curl-influx.sh
+++ scripts/curl-influx.sh
@@ -3,14 +3,23 @@
 server=192.168.100.124
 # curl -i -X POST "http://${server}:8086/query" -u "henninb:monday1" --data-urlencode "q=CREATE DATABASE metrics"
 
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT * FROM method_timed" > metrics-method_timed.json
 
-curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value FROM stuff" > stuff-values.json
-
-curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW SERIES ON metrics" > metrics-series.json
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW series ON metrics" > metrics-series.json
 
 curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value from metrics" > metrics-values.json
-curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SELECT value from metrics-func" > metrics-func_values.json
 
-curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW measurements on metrics" > metrics-measurements.json
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=SHOW measurements ON metrics" > metrics-measurements.json
+
+curl -s -G "http://${server}:8086/query?pretty=true" --data-urlencode "db=metrics" -u "henninb:monday1" --data-urlencode "q=select * from transaction_insert_timed" > transactions-inserted.json
+
+# drop series from TRANSACTION_LIST_IS_EMPTY;
+# drop series from TRANSACTION_RECEIPT_IMAGE;
+# drop series from TRANSACTION_RECEIVED_EVENT_COUNTER;
+# drop series from TRANSACTION_SUCCESSFULLY_INSERTED_COUNTER;
+# drop series from TRANSACTION_UPDATE_CLEARED_COUNTER;
+# drop series from exception_counter;
+# select * from method_timed;
+# select mean from method_timed where mean > 0;
 
 exit 0
--- src/main/kotlin/finance/configurations/BeanConfig.kt
+++ src/main/kotlin/finance/configurations/BeanConfig.kt
@@ -18,41 +18,8 @@ open class BeanConfig {
     }
 
     @Bean
-    open fun timedAspect(registry: MeterRegistry): TimedAspect {
-        return TimedAspect(registry)
+    open fun timedAspect(meterRegistry: MeterRegistry): TimedAspect {
+        return TimedAspect(meterRegistry)
     }
 
-//    @Bean
-//    open fun objectMapper(): ObjectMapper {
-//        val objectMapper = ObjectMapper()
-//        val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd")
-//        objectMapper.dateFormat = dateFormat
-//        //objectMapper.setTimeZone(TimeZone.getTimeZone("America/Chicago"))
-//        objectMapper.setTimeZone(TimeZone.getDefault())
-//        return objectMapper
-//    }
-
-//    @Bean
-//    open fun servletContainerFactory(): EmbeddedServletContainerFactory {
-//        val factory = TomcatEmbeddedServletContainerFactory()
-//        factory.addConnectorCustomizers(TomcatConnectorCustomizer { connector ->
-//            connector.setAttribute("sslProtocols", "TLSv1.1,TLSv1.2")
-//            connector.setAttribute("sslEnabledProtocols", "TLSv1.1,TLSv1.2")
-//        })
-//        return factory
-//    }
-
-
-//    @Bean
-//    open fun containerCustomizer() :  EmbeddedServletContainerCustomizer {
-//        val container
-//        return (container -> {
-//            container.setPort(8080);
-//        })
-//    }
-
-    //TODO: fix this bug
-    //NioEndpoint - Error running socket processor, java.lang.NullPointerException
-    //WORKAROUND: Disable TLSv1.3 and running with TLSv1.2 only. Or use OpenSSL for the encryption.
-    //I simply changed the connector protocol from org.apache.coyote.http11.Http11Protocol to org.apache.coyote.http11.Http11NioProtocol
 }
