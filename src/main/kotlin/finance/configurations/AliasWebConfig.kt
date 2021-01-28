c1f03de57f2f408f183cbfabd5827f5d6b88f4de
Brian Henning <henninb@msn.com>
2021-01-28 16:39:04 -0600

    version updates

---
+++ src/main/kotlin/finance/configurations/AliasWebConfig.kt
@@ -0,0 +1,12 @@
+package finance.configurations
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class AliasWebConfig  : WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/health").setViewName("forward:/actuator/health");
+    }
+}
