36864c861df6c5fdee36487320e214f4a8e0dd0f
Brian Henning <henninb@msn.com>
2021-01-27 18:42:58 -0600

    cleanup tests

--- src/test/unit/groovy/finance/services/PaymentServiceSpec.groovy
+++ src/test/unit/groovy/finance/services/PaymentServiceSpec.groovy
@@ -8,10 +8,11 @@ import finance.domain.Transaction
 import finance.helpers.AccountBuilder
 import finance.helpers.PaymentBuilder
 import finance.helpers.TransactionBuilder
-import spock.lang.Ignore
+import finance.utils.Constants
 
 import javax.validation.ConstraintViolation
 
+@SuppressWarnings("GroovyAccessibility")
 class PaymentServiceSpec extends BaseServiceSpec {
 
     void 'test findAll payments empty'() {
@@ -29,18 +30,15 @@ class PaymentServiceSpec extends BaseServiceSpec {
         0 * _
     }
 
-    @Ignore('can be fixed')
     void 'test insertPayment - existing'() {
         given:
         Payment payment = PaymentBuilder.builder().withAmount(5.0).build()
+        Transaction transaction = TransactionBuilder.builder().build()
         Account account = AccountBuilder.builder().build()
         Parameter parameter = new Parameter()
         parameter.parameterValue = 'val'
         parameter.parameterName = 'payment_account'
-        Transaction transactionCreditLocal
-        Transaction transactionDebitLocal
         Set<ConstraintViolation<Payment>> constraintViolations = validator.validate(payment)
-
 
         when:
         Boolean isInserted = paymentService.insertPayment(payment)
@@ -48,26 +46,26 @@ class PaymentServiceSpec extends BaseServiceSpec {
         then:
         isInserted.is(true)
         1 * accountRepositoryMock.findByAccountNameOwner(account.accountNameOwner) >> Optional.of(account)
-        1 * transactionServiceMock.insertTransaction({ Transaction transactionDebit ->
-            transactionDebitLocal = transactionDebit
+        1 * parameterRepositoryMock.findByParameterName(parameter.parameterName) >> Optional.of(parameter)
+        1 * validatorMock.validate(_ as Payment) >> constraintViolations
+        1 * validatorMock.validate({ Transaction transactionDebit ->
             assert transactionDebit.category == 'bill_pay'
             assert transactionDebit.description == 'payment'
             assert transactionDebit.notes == 'to ' + payment.accountNameOwner
             assert transactionDebit.amount == (payment.amount * -1.0)
             assert transactionDebit.accountType == AccountType.Debit
-        })
-        1 * transactionServiceMock.insertTransaction({ Transaction transactionCredit ->
-            transactionCreditLocal = transactionCredit
+        }) >> [].toSet()
+        1 * validatorMock.validate({ Transaction transactionCredit ->
             assert transactionCredit.category == 'bill_pay'
             assert transactionCredit.description == 'payment'
             assert transactionCredit.notes == 'from ' + parameter.parameterValue
             assert transactionCredit.amount == (payment.amount * -1.0)
             assert transactionCredit.accountType == AccountType.Credit
-        })
-        1 * parameterRepositoryMock.findByParameterName(parameter.parameterName) >> Optional.of(parameter)
-        1 * validatorMock.validate(_ as Payment) >> constraintViolations
-        1 * validatorMock.validate(_ as Transaction) >> [].toSet()
-        1 * transactionRepositoryMock.findByGuid(_ as String)
+        }) >> [].toSet()
+        2 * transactionRepositoryMock.findByGuid(_ as String) >> Optional.of(transaction)
+        1 * meterRegistryMock.counter(setMeterId(Constants.TRANSACTION_ALREADY_EXISTS_COUNTER, transaction.accountNameOwner)) >> counter
+        1 * meterRegistryMock.counter(setMeterId(Constants.TRANSACTION_ALREADY_EXISTS_COUNTER, transaction.accountNameOwner)) >> counter
+        2 * counter.increment()
         1 * paymentRepositoryMock.saveAndFlush(payment)
         0 * _
     }
