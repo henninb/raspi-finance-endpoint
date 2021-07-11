71c257204e0eddbc073db7da2f83624621efad2d
Brian Henning <henninb@msn.com>
2021-07-11 15:07:46 -0500

    wip

--- src/main/kotlin/finance/controllers/ValidationAmountController.kt
+++ src/main/kotlin/finance/controllers/ValidationAmountController.kt
@@ -2,8 +2,10 @@ package finance.controllers
 
 import finance.domain.ValidationAmount
 import finance.services.ValidationAmountService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @CrossOrigin
 @RestController
@@ -15,5 +17,15 @@ class ValidationAmountController(private var validationAmountService: Validation
         validationAmountService.insertValidationAmount(validationAmount)
         logger.info("validationAmount inserted")
         return ResponseEntity.ok("validationAmount inserted")
+    }
+
+    @GetMapping("/select/{accountId}")
+    fun selectValidationAmountByAccountId(@PathVariable("accountId") accountId: Long): ResponseEntity<String> {
+        val validationAmountOptional = validationAmountService.findByAccountId(accountId)
+
+        if (validationAmountOptional.isPresent) {
+            return ResponseEntity.ok(mapper.writeValueAsString(validationAmountOptional.get()))
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND, "validationAmount not found for: $accountId")
     }
 }
