5e828feef8f7d37b23ccafb1176df06cde024089
Brian Henning <henninb@msn.com>
2021-07-11 14:41:48 -0500

    added insert feature

--- src/main/kotlin/finance/repositories/ValidationAmountRepository.kt
+++ src/main/kotlin/finance/repositories/ValidationAmountRepository.kt
@@ -2,6 +2,8 @@ package finance.repositories
 
 import finance.domain.ValidationAmount
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface ValidationAmountRepository : JpaRepository<ValidationAmount, Long> {
+    fun findByValidationId(accountId: Long): Optional<ValidationAmount>
 }
