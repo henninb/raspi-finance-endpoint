3824628a0e802a2c360d1aea1e64189d9b941f68
Brian Henning <henninb@msn.com>
2021-07-14 18:36:48 -0500

    added graphql logic

--- src/main/kotlin/finance/configurations/GraphqlValidationConfig.kt
+++ src/main/kotlin/finance/configurations/GraphqlValidationConfig.kt
@@ -1,20 +1,34 @@
 package finance.configurations
 
-import graphql.schema.idl.SchemaDirectiveWiring
-import graphql.validation.rules.ValidationRules
-import graphql.validation.schemawiring.ValidationSchemaWiring
+import finance.controllers.resolver.CategoryQueryResolver
+import finance.controllers.resolver.DescriptionQueryResolver
+import graphql.Scalars
+import graphql.kickstart.tools.SchemaParser
+import graphql.schema.GraphQLScalarType
+import graphql.schema.GraphQLSchema
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.sql.Timestamp
+
 
 @Configuration
-open class GraphQLValidationConfig {
+open class GraphqlValidationConfig {
+
+//    @Bean
+//    open fun timestampType(): GraphQLScalarType {
+//        return Timestamp
+//    }
 
     @Bean
-    open fun directiveWiring(): SchemaDirectiveWiring {
-        val validationRules = ValidationRules.newValidationRules()
-            //.onValidationErrorStrategy(ThrowExceptionStrategy())
-            //.addRule(CheckWildcardConstraint())
+    open fun graphqlSchema(
+        descriptionQueryResolver: DescriptionQueryResolver,
+        categoryQueryResolver: CategoryQueryResolver
+
+    ): GraphQLSchema {
+        return SchemaParser.newParser()
+            .file("graphql/description.graphqls")
+            .resolvers(descriptionQueryResolver, categoryQueryResolver)
             .build()
-        return ValidationSchemaWiring(validationRules)
+            .makeExecutableSchema()
     }
 }
