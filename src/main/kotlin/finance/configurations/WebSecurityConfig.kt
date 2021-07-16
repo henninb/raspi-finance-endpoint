641479a1cbb9888f8e6f5bebce5cd684c067e29f
Brian Henning <henninb@msn.com>
2021-07-16 07:41:49 -0500

    cors fix

--- src/main/kotlin/finance/configurations/WebSecurityConfig.kt
+++ src/main/kotlin/finance/configurations/WebSecurityConfig.kt
@@ -1,9 +1,14 @@
 package finance.configurations
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +16,17 @@ open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     // TODO: disable security temporarily
     @Throws(java.lang.Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
+    }
+
+    @Bean
+    open fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = mutableListOf("*", "https://localhost:3000")
+        configuration.allowedMethods = mutableListOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = mutableListOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
