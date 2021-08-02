69feb17e9b0131b1f117fbb79491c172d02ea2ef
Brian Henning <henninb@msn.com>
2021-08-01 19:24:30 -0500

    updates

---
+++ jarrun.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm -rf env.bootrun
+sed "s/\/opt\/raspi-finance-endpoint/./g" env.prod > env.bootrun
+# sed -i "s/INFLUXDB_ENABLED=true/INFLUXDB_ENABLED=false/g" env.bootrun
+
+set -a
+# shellcheck disable=SC1091
+source env.bootrun
+# shellcheck disable=SC1091
+source env.secrets
+set +a
+
+# ./gradlew clean build bootRun -x test
+java -jar build/libs/raspi-finance-endpoint.jar
+
+exit 0
