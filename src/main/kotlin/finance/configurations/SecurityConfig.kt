26219f6623e69792b23c304ad601ab80d531ae0e
Brian Henning <henninb@msn.com>
2021-11-24 08:48:47 -0600

    wip

--- src/main/kotlin/finance/configurations/SecurityConfig.kt
+++ src/main/kotlin/finance/configurations/SecurityConfig.kt
@@ -4,26 +4,21 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-@Configuration
-open class SecurityConfig : WebSecurityConfigurerAdapter() {
-    @Throws(Exception::class)
-    public override fun configure(http: HttpSecurity) {
-        http
-            .cors()
-            .and()
-            .authorizeRequests()
-            //.antMatchers("/").permitAll()
-            //.antMatchers("/graphql").authenticated()
-            .anyRequest().permitAll()
-            .and().httpBasic()
+//@Configuration
+//open class SecurityConfig : WebSecurityConfigurerAdapter() {
+//    @Throws(Exception::class)
+//    public override fun configure(http: HttpSecurity) {
+//        http
+//            .cors()
 //            .and()
-//            .sessionManagement()
-//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .csrf()
-            .disable()
-//            .csrf().ignoringAntMatchers("/graphql")
-        http.authorizeRequests().anyRequest().permitAll()
-        // http.authorizeRequests().anyRequest().authenticated();
-    }
-}
+//            .authorizeRequests()
+//            .anyRequest().permitAll()
+//            .and().httpBasic()
+//            .and()
+//            .csrf()
+//            .disable()
+//
+//        //http.authorizeRequests().anyRequest().permitAll()
+//        // http.authorizeRequests().anyRequest().authenticated()
+//    }
+//}
