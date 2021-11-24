9805e96d936a180f9674f12741c2b589a0c6d1b1
Brian Henning <henninb@msn.com>
2021-11-24 08:53:56 -0600

    wip

--- src/main/kotlin/finance/configurations/SecurityConfig.kt
+++ src/main/kotlin/finance/configurations/SecurityConfig.kt
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-//@Configuration
-//open class SecurityConfig : WebSecurityConfigurerAdapter() {
+@Configuration
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
 //    @Throws(Exception::class)
 //    public override fun configure(http: HttpSecurity) {
 //        http
@@ -21,4 +21,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 //        //http.authorizeRequests().anyRequest().permitAll()
 //        // http.authorizeRequests().anyRequest().authenticated()
 //    }
-//}
+
+    @Throws(java.lang.Exception::class)
+    override fun configure(security: HttpSecurity) {
+        security.httpBasic().disable()
+    }
+}
