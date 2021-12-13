bfde7c5b8c8586a3a2fc8c8a0de0a4f40a69ef38
Brian Henning <henninb@msn.com>
2021-12-13 06:05:09 -0600

    wip

--- src/test/unit/groovy/finance/processors/InsertTransactionProcessorSpec.groovy
+++ src/test/unit/groovy/finance/processors/InsertTransactionProcessorSpec.groovy
@@ -56,56 +56,55 @@ class InsertTransactionProcessorSpec extends BaseProcessorSpec {
         0 * _
     }
 
-    @Ignore
-    void 'test -- InsertTransactionProcessor - invalid record'() {
-        given:
-        Set<ConstraintViolation<Transaction>> constraintViolations = validator.validate(new Transaction())
-        exchange.in.setBody('{}')
+//    @Ignore
+//    void 'test -- InsertTransactionProcessor - invalid record'() {
+//        given:
+//        Set<ConstraintViolation<Transaction>> constraintViolations = validator.validate(new Transaction())
+//        exchange.in.setBody('{}')
+//
+//        when:
+//        insertTransactionProcessor.process(exchange)
+//
+//        then:
+//        thrown(ValidationException)
+//        1 * transactionServiceMock.insertTransaction(new Transaction())
+//        //1 * validatorMock.validate(new Transaction()) >> constraintViolations
+//        1 * meterRegistryMock.counter(validationExceptionThrownMeter) >> counter
+//        //1 * counter.increment()
+//        0 * _
+//    }
 
-        when:
-        insertTransactionProcessor.process(exchange)
-
-        then:
-        thrown(ValidationException)
-        1 * transactionServiceMock.insertTransaction(new Transaction())
-        //1 * validatorMock.validate(new Transaction()) >> constraintViolations
-        1 * meterRegistryMock.counter(validationExceptionThrownMeter) >> counter
-        //1 * counter.increment()
-        0 * _
-    }
-
-
-    @Ignore("need to fix")
-    void 'test -- InsertTransactionProcessor - valid'() {
-        given:
-        //Transaction transaction = mapper.readValue(jsonPayload, Transaction)
-        Transaction transaction = TransactionBuilder.builder().withAccountNameOwner("chase_brain").build()
-        //transaction.accountNameOwner= 'chase_brain'
-        Account account = AccountBuilder.builder().build()
-        Category category = CategoryBuilder.builder().build()
-        account.accountNameOwner = transaction.accountNameOwner
-        category.categoryName = transaction.category
-        Set<ConstraintViolation<Transaction>> constraintViolations = validator.validate(transaction)
-        Set<ConstraintViolation<Category>> constraintViolationsCategory = validator.validate(category)
-        Set<ConstraintViolation<Account>> constraintViolationsAccount = validator.validate(account)
-        exchange.in.setBody(jsonPayload)
-
-        when:
-        insertTransactionProcessor.process(exchange)
-
-        then:
-        1 * transactionRepositoryMock.findByGuid(transaction.guid) >> Optional.empty()
-        1 * accountRepositoryMock.findByAccountNameOwner(transaction.accountNameOwner) >> Optional.empty()
-        2 * accountRepositoryMock.findByAccountNameOwner(transaction.accountNameOwner) >> Optional.of(account)
-        1 * mockCategoryRepository.findByCategoryName(transaction.category) >> Optional.empty()
-        1 * mockCategoryRepository.saveAndFlush(category) >> category
-        //1 * validatorMock.validate(transaction) >> constraintViolations
-        1 * validatorMock.validate(account) >> constraintViolationsAccount
-        1 * validatorMock.validate(category) >> constraintViolationsCategory
-        1 * transactionRepositoryMock.saveAndFlush(transaction)
-        1 * meterRegistryMock.counter(setMeterId(Constants.TRANSACTION_SUCCESSFULLY_INSERTED_COUNTER, transaction.accountNameOwner)) >> counter
-        1 * meterRegistryMock.counter(setMeterId(Constants.CAMEL_TRANSACTION_SUCCESSFULLY_INSERTED_COUNTER, transaction.accountNameOwner)) >> counter
-        2 * counter.increment()
-        0 * _
-    }
+//    @Ignore("need to fix")
+//    void 'test -- InsertTransactionProcessor - valid'() {
+//        given:
+//        //Transaction transaction = mapper.readValue(jsonPayload, Transaction)
+//        Transaction transaction = TransactionBuilder.builder().withAccountNameOwner("chase_brain").build()
+//        //transaction.accountNameOwner= 'chase_brain'
+//        Account account = AccountBuilder.builder().build()
+//        Category category = CategoryBuilder.builder().build()
+//        account.accountNameOwner = transaction.accountNameOwner
+//        category.categoryName = transaction.category
+//        Set<ConstraintViolation<Transaction>> constraintViolations = validator.validate(transaction)
+//        Set<ConstraintViolation<Category>> constraintViolationsCategory = validator.validate(category)
+//        Set<ConstraintViolation<Account>> constraintViolationsAccount = validator.validate(account)
+//        exchange.in.setBody(jsonPayload)
+//
+//        when:
+//        insertTransactionProcessor.process(exchange)
+//
+//        then:
+//        1 * transactionRepositoryMock.findByGuid(transaction.guid) >> Optional.empty()
+//        1 * accountRepositoryMock.findByAccountNameOwner(transaction.accountNameOwner) >> Optional.empty()
+//        2 * accountRepositoryMock.findByAccountNameOwner(transaction.accountNameOwner) >> Optional.of(account)
+//        1 * mockCategoryRepository.findByCategoryName(transaction.category) >> Optional.empty()
+//        1 * mockCategoryRepository.saveAndFlush(category) >> category
+//        //1 * validatorMock.validate(transaction) >> constraintViolations
+//        1 * validatorMock.validate(account) >> constraintViolationsAccount
+//        1 * validatorMock.validate(category) >> constraintViolationsCategory
+//        1 * transactionRepositoryMock.saveAndFlush(transaction)
+//        1 * meterRegistryMock.counter(setMeterId(Constants.TRANSACTION_SUCCESSFULLY_INSERTED_COUNTER, transaction.accountNameOwner)) >> counter
+//        1 * meterRegistryMock.counter(setMeterId(Constants.CAMEL_TRANSACTION_SUCCESSFULLY_INSERTED_COUNTER, transaction.accountNameOwner)) >> counter
+//        2 * counter.increment()
+//        0 * _
+//    }
 }
