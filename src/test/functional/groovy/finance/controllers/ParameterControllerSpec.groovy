210ef57e6435c821168321a0122897d9b328169f
Brian Henning <henninb@msn.com>
2021-12-16 05:54:04 -0600

    wip

--- src/test/functional/groovy/finance/controllers/ParameterControllerSpec.groovy
+++ src/test/functional/groovy/finance/controllers/ParameterControllerSpec.groovy
@@ -3,14 +3,15 @@ package finance.controllers
 import finance.Application
 import finance.domain.Parameter
 import finance.helpers.ParameterBuilder
+import groovy.util.logging.Log
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
-import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Stepwise
 
+@Log
 @Stepwise
 @ActiveProfiles("func")
 @SpringBootTest(classes = Application, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,29 +26,57 @@ class ParameterControllerSpec extends BaseControllerSpec {
     @Shared
     protected String endpointName = 'parm'
 
-//    void setup() {
-//
-//    }
+    void setup() {
+        log.info("setup")
+        ResponseEntity<String> response = selectEndpoint(endpointName, parameter.parameterName)
+        if (response.statusCode == HttpStatus.OK) {
+            deleteEndpoint(endpointName, parameter.parameterName)
+            log.info("delete ${parameter.toString()}")
+        }
+    }
 
-    void 'test insert Parameter'() {
-        when:
-        ResponseEntity<String> response = insertEndpoint(endpointName, parameter.toString())
+    void 'test insert, find, delete Parameter'() {
+        given:
+        ResponseEntity<String> response
+
+        when: 'insert'
+        response = insertEndpoint(endpointName, parameter.toString())
+
+        then:
+        response.statusCode == HttpStatus.OK
+        response.body.contains(parameter.parameterValue)
+        0 * _
+
+        when: 'find found'
+        response = selectEndpoint(endpointName, parameter.parameterName)
 
         then:
         response.statusCode == HttpStatus.OK
         0 * _
-    }
 
-    void 'test insert Parameter - duplicate'() {
-        when:
-        ResponseEntity<String> response = insertEndpoint(endpointName, parameter.toString())
+        when: 'attempt duplicate insert'
+        response = insertEndpoint(endpointName, parameter.toString())
 
         then:
         response.statusCode == HttpStatus.BAD_REQUEST
         0 * _
+
+        when: 'delete parameter'
+        response = deleteEndpoint(endpointName, parameter.parameterName)
+
+        then:
+        response.statusCode == HttpStatus.OK
+        0 * _
+
+        when: 'find not found after removal'
+        response = selectEndpoint(endpointName, parameter.parameterName)
+
+        then:
+        response.statusCode == HttpStatus.NOT_FOUND
+        0 * _
     }
 
-    void 'test find parameter - not found'() {
+    void 'find parameter - not found'() {
         when:
         ResponseEntity<String> response = selectEndpoint(endpointName, UUID.randomUUID().toString())
 
@@ -56,35 +85,7 @@ class ParameterControllerSpec extends BaseControllerSpec {
         0 * _
     }
 
-    void 'test find parameter'() {
-        when:
-        ResponseEntity<String> response = selectEndpoint(endpointName, parameter.parameterName)
-
-        then:
-        response.statusCode == HttpStatus.OK
-        0 * _
-    }
-
-    void 'test delete parameter'() {
-        when:
-        ResponseEntity<String> response = deleteEndpoint(endpointName, parameter.parameterName)
-
-        then:
-        response.statusCode == HttpStatus.OK
-        0 * _
-    }
-
-    void 'test find description - not found after removal'() {
-        when:
-        ResponseEntity<String> response = selectEndpoint(endpointName, parameter.parameterName)
-
-        then:
-        response.statusCode == HttpStatus.NOT_FOUND
-        0 * _
-    }
-
-    @Ignore('should return a 404 NOT_FOUND')
-    void 'test delete parameter - not found'() {
+    void 'delete parameter - not found'() {
         when:
         ResponseEntity<String> response = deleteEndpoint(endpointName, UUID.randomUUID().toString())
 
