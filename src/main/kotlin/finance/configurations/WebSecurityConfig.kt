76e24848f8285dd7544defb08837e4c5598a6c57
Brian Henning <henninb@msn.com>
2022-02-18 06:57:26 -0600

    wip

--- src/main/kotlin/finance/configurations/WebSecurityConfig.kt
+++ src/main/kotlin/finance/configurations/WebSecurityConfig.kt
@@ -2,19 +2,20 @@ package finance.configurations
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import javax.servlet.http.HttpServletRequest
 
 @Configuration
 open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     @Throws(java.lang.Exception::class)
     override fun configure(http: HttpSecurity) {
         val corsConfiguration = CorsConfiguration()
+
+
 
         //corsConfiguration.allowedOrigins = mutableListOf("*")
         corsConfiguration.allowedOriginPatterns = mutableListOf("*")
@@ -24,13 +25,33 @@ open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
         corsConfiguration.exposedHeaders = mutableListOf("x-auth-token")
         corsConfiguration.allowedHeaders = mutableListOf("authorization", "content-type", "x-auth-token", "accept");
 
-        //TODO: enable csrf (cross site request forgery)
-        //TODO: enable headers (not sure what happens when they are disabled)
-        http.csrf().disable().cors()
+        // TODO: bh enable csrf (cross site request forgery)
+        http.csrf().disable()
+
+        // No session will be created or used by spring security
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+//        // Entry points
+//        http.authorizeRequests() //
+//            .antMatchers("/user/signin").permitAll()
+//            .antMatchers("/user/signup").permitAll()
+//            // Disallow everything else
+//            .anyRequest().authenticated()
+
+        // TODO: bh enable headers (not sure what happens when they are disabled) preflight
+        http.cors()
             .configurationSource { corsConfiguration }.and().headers().disable()
 
 //        http.cors()
 //            .configurationSource { corsConfiguration }
 
+        http.httpBasic()
+
     }
+
+//    @Bean
+//    open fun passwordEncoder(): PasswordEncoder {
+//        return BCryptPasswordEncoder(12)
+//    }
+
 }
