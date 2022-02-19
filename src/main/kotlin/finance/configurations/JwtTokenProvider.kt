4f9e3bdc92d1b92f5a95c0b3cee8e6d5acec34d5
Brian Henning <henninb@msn.com>
2022-02-19 17:19:22 -0600

    wip

---
+++ src/main/kotlin/finance/configurations/JwtTokenProvider.kt
@@ -0,0 +1,88 @@
+package finance.configurations
+
+import finance.services.JwtUserDetailService
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.annotation.PostConstruct
+import javax.servlet.http.HttpServletRequest
+
+
+@Component
+open class JwtTokenProvider(myUserDetails: JwtUserDetailService) {
+    /**
+     * THIS IS NOT A SECURE PRACTICE! For simplicity, we are storing a static key here. Ideally, in a
+     * microservices environment, this key would be kept on a config-server.
+     */
+    //@Value("\${security.jwt.token.secret-key:secret-key}")
+    //private var secretKey: String? = null
+    private var secretKey: String = "secretKey"
+
+    //@Value("\${security.jwt.token.expire-length:3600000}")
+    //private val validityInMilliseconds: Long = 3600000 // 1h
+    private val validityInMilliseconds: Long = 3600000 // 1h
+
+    private val jwtUserDetailService: JwtUserDetailService
+
+    @PostConstruct
+    protected fun init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+    }
+
+//    fun createToken(username: String?, appUserRoles: List<AppUserRole>): String {
+//        val claims: Claims = Jwts.claims().setSubject(username)
+//        claims.put("auth", appUserRoles.stream().map(Function<AppUserRole, SimpleGrantedAuthority> { s: AppUserRole ->
+//            SimpleGrantedAuthority(
+//                s.getAuthority()
+//            )
+//        }).filter { obj: SimpleGrantedAuthority? ->
+//            Objects.nonNull(
+//                obj
+//            )
+//        }.collect(Collectors.toList()))
+//        val now = Date()
+//        val validity = Date(now.time + validityInMilliseconds)
+//        return Jwts.builder() //
+//            .setClaims(claims) //
+//            .setIssuedAt(now) //
+//            .setExpiration(validity) //
+//            .signWith(SignatureAlgorithm.HS256, secretKey) //
+//            .compact()
+//    }
+
+    fun getAuthentication(token: String?): Authentication {
+        val userDetails: UserDetails = jwtUserDetailService.loadUserByUsername(getUsername(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    fun getUsername(token: String?): String {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject()
+    }
+
+    fun resolveToken(req: HttpServletRequest): String? {
+        val bearerToken = req.getHeader("Authorization")
+        return if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            bearerToken.substring(7)
+        } else null
+    }
+
+    fun validateToken(token: String?): Boolean {
+        return try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+            true
+        } catch (e: JwtException) {
+            throw RuntimeException("Expired or invalid JWT token")
+            //throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (e: IllegalArgumentException) {
+            throw RuntimeException("Expired or invalid JWT token")
+        }
+    }
+
+    init {
+        this.jwtUserDetailService = myUserDetails
+    }
+}
--- src/main/kotlin/finance/repositories/UserRepository.kt
+++ src/main/kotlin/finance/repositories/UserRepository.kt
@@ -5,4 +5,5 @@ import finance.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
+    fun findByUsername(username: String): User?
 }
