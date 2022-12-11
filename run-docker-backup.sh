cf778a5a6c462eacf3943ed390b1643b35c737a7
Brian Henning <henninb@msn.com>
2022-12-11 11:27:18 -0600

    wip

--- run-docker-backup.sh
+++ run-docker-backup.sh
@@ -7,4 +7,10 @@ docker save -o raspi-finance-react-${date}.tar raspi-finance-react
 echo docker load -i raspi-finance-endpoint-${date}.tar
 echo docker load -i raspi-finance-react-${date}.tar
 
+scp raspi-finance-endpoint-${date}.tar henninb@192.168.10.10:/home/henninb
+scp raspi-finance-react-${date}.tar henninb@192.168.10.10:/home/henninb
+
+echo docker run -dit --restart unless-stopped -p 8443:8443 --name raspi-finance-endpoint -h raspi-finance-endpoint raspi-finance-endpoint
+echo docker run -dit --restart unless-stopped -p 3000:443 --name raspi-finance-react -h raspi-finance-react raspi-finance-react
+
 exit 0
