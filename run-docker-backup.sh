5a6a2ced7a43fb96a33a1a2e85fce98968d2fc6c
Brian Henning <henninb@msn.com>
2022-12-27 14:06:08 -0600

    wip

--- run-docker-backup.sh
+++ run-docker-backup.sh
@@ -10,7 +10,7 @@ echo docker load -i raspi-finance-react-${date}.tar
 scp raspi-finance-endpoint-${date}.tar henninb@192.168.10.10:/home/henninb
 scp raspi-finance-react-${date}.tar henninb@192.168.10.10:/home/henninb
 
-echo docker run -dit --restart unless-stopped -p 8443:8443 --name raspi-finance-endpoint -h raspi-finance-endpoint raspi-finance-endpoint
+echo docker run -dit --restart unless-stopped  --add-host raspi:192.168.10.25 -p 8443:8443 --env-file env.secrets --env-file env.prod --name raspi-finance-endpoint -h raspi-finance-endpoint raspi-finance-endpoint
 echo docker run -dit --restart unless-stopped -p 3000:443 --name raspi-finance-react -h raspi-finance-react raspi-finance-react
 
 exit 0
