07a77463268c2ef93051da7a6453737595095900
Brian Henning <henninb@msn.com>
2022-12-28 17:52:12 -0600

    added in web security

--- src/main/kotlin/finance/configurations/WebSecurityConfig.kt
+++ src/main/kotlin/finance/configurations/WebSecurityConfig.kt
@@ -3,7 +3,6 @@ package finance.configurations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 
@@ -15,16 +14,13 @@ open class WebSecurityConfig()  {
     open fun configure(http: HttpSecurity) : SecurityFilterChain {
         val corsConfiguration = CorsConfiguration()
 
-        corsConfiguration.allowedOrigins = mutableListOf("https://hornsup:3000", "https://localhost:3000", "https://localhost:3000")
+        //corsConfiguration.allowedOrigins = mutableListOf("http://localhost", "https://localhost:3000", "https://localhost")
         corsConfiguration.allowedOriginPatterns = mutableListOf("*")
         corsConfiguration.allowedMethods = mutableListOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         //corsConfiguration.allowedHeaders = mutableListOf("*")
         corsConfiguration.allowCredentials = true // this line is important it sends only specified domain instead of *
         corsConfiguration.exposedHeaders = mutableListOf("x-auth-token")
         corsConfiguration.allowedHeaders = mutableListOf("authorization", "content-type", "x-auth-token", "accept");
-
-        // TODO: bh enable csrf (cross site request forgery)
-        //http.csrf().disable()
 
         // No session will be created or used by spring security
         // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -36,26 +32,13 @@ open class WebSecurityConfig()  {
 //            // Disallow everything else
 //            .anyRequest().authenticated()
 
+        // TODO: bh enable csrf (cross site request forgery)
         // TODO: bh enable headers (not sure what happens when they are disabled) preflight
-        http.cors()
-            .configurationSource { corsConfiguration }.and().headers().disable()
-
-        //http.httpBasic()
+        http.httpBasic().and()
+            .cors()
+            .configurationSource { corsConfiguration }.and()
+            .headers().disable()
             .csrf().disable()
         return http.build()
     }
-
-
-//    @Bean
-//    @Throws(Exception::class)
-//    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
-//        http.authorizeRequests()
-//            .anyRequest()
-//            .authenticated()
-//            .and()
-//            .httpBasic()
-//        return http.build()
-//    }
-
-
 }
