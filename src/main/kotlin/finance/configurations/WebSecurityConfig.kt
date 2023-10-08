de90e180184a54cf3c5b0c1902c1e70adbff1039
Brian Henning <henninb@msn.com>
2023-10-08 14:41:51 -0500

    wip

--- src/main/kotlin/finance/configurations/WebSecurityConfig.kt
+++ src/main/kotlin/finance/configurations/WebSecurityConfig.kt
@@ -3,7 +3,10 @@ package finance.configurations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
@@ -27,27 +30,69 @@ open class WebSecurityConfig( private val environment: Environment)  {
 //    open fun passwordEncoder(): PasswordEncoder {
 //        return BCryptPasswordEncoder()
 //    }
+//    @Bean
+//    open fun configure(http: HttpSecurity) : SecurityFilterChain {
+//        // TODO: bh enable csrf (cross site request forgery)
+//        // TODO: bh how to enable basic auth
+//        http
+//            .csrf().disable()
+//            .authorizeHttpRequests()
+//            .anyRequest()
+//            .authenticated()
+//            //.and()
+//            //.oauth2ResourceServer().jwt()
+//            //.httpBasic()
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // the server will not send a JSESSIONID cookie
+//
+//        return http.build()
+//    }
+
     @Bean
-    open fun configure(http: HttpSecurity) : SecurityFilterChain {
-//        val username = environment.getProperty("spring.security.user.name")
-//        val password = environment.getProperty("spring.security.user.password")
-//        println("U=$username")
-//        println("P=${password}")
-
-        // TODO: bh enable csrf (cross site request forgery)
-        // TODO: bh how to enable basic auth
-        http
-            .csrf().disable()
-            .authorizeHttpRequests()
-            .anyRequest()
-            .authenticated()
-            //.and()
-            //.oauth2ResourceServer().jwt()
-            //.httpBasic()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // the server will not send a JSESSIONID cookie
-
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests().requestMatchers("/**").hasRole("USER").and().formLogin()
         return http.build()
     }
+
+
+
+//    @Bean
+//    fun userDetailsService(): UserDetailsService {
+//        val user: UserDetails = User.withDefaultPasswordEncoder()
+//            .username("user")
+//            .password("password")
+//            .roles("USER")
+//            .build()
+//        val admin: UserDetails = User.withDefaultPasswordEncoder()
+//            .username("admin")
+//            .password("password")
+//            .roles("ADMIN", "USER")
+//            .build()
+//        return InMemoryUserDetailsManager(user, admin)
+//    }
+
+//    @Bean
+//    fun userDetailsService(): InMemoryUserDetailsManager {
+//        val user: UserDetails = User.withDefaultPasswordEncoder()
+//            .username("user")
+//            .password("password")
+//            .roles("USER")
+//            .build()
+//        return InMemoryUserDetailsManager(user)
+//    }
+
+//    @Bean
+//    open fun configure(http: HttpSecurity): SecurityFilterChain {
+//        http
+//            .csrf().disable() // Disable CSRF protection (only do this for trusted environments)
+//            .authorizeHttpRequests()
+//            .antMatchers("/**").permitAll() // Allow all requests without authentication
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//        return http.build()
+//    }
 }
