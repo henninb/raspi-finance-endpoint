2000e269394b916985d8c404afd509f3d391dc18
Brian Henning <henninb@msn.com>
2025-01-26 08:37:54 -0600

    wip

--- src/main/kotlin/finance/services/ValidationAmountService.kt
+++ src/main/kotlin/finance/services/ValidationAmountService.kt
@@ -16,6 +16,26 @@ open class ValidationAmountService(
     private var accountRepository: AccountRepository
 ) : IValidationAmountService, BaseService() {
 
+//    override fun insertValidationAmount(
+//        accountNameOwner: String,
+//        validationAmount: ValidationAmount
+//    ): ValidationAmount {
+//        var accountId = 0L
+//        val accountOptional = accountRepository.findByAccountNameOwner(accountNameOwner)
+//        if (accountOptional.isPresent) {
+//            accountId = accountOptional.get().accountId
+//        }
+//
+//        val constraintViolations: Set<ConstraintViolation<ValidationAmount>> = validator.validate(validationAmount)
+//        handleConstraintViolations(constraintViolations, meterService)
+//        validationAmount.accountId = accountId
+//        validationAmount.dateAdded = Timestamp(Calendar.getInstance().time.time)
+//        validationAmount.dateUpdated = Timestamp(Calendar.getInstance().time.time)
+//        return validationAmountRepository.saveAndFlush(validationAmount)
+//    }
+
+
+
     override fun insertValidationAmount(
         accountNameOwner: String,
         validationAmount: ValidationAmount
@@ -28,10 +48,23 @@ open class ValidationAmountService(
 
         val constraintViolations: Set<ConstraintViolation<ValidationAmount>> = validator.validate(validationAmount)
         handleConstraintViolations(constraintViolations, meterService)
+
         validationAmount.accountId = accountId
         validationAmount.dateAdded = Timestamp(Calendar.getInstance().time.time)
         validationAmount.dateUpdated = Timestamp(Calendar.getInstance().time.time)
-        return validationAmountRepository.saveAndFlush(validationAmount)
+
+        // Save the ValidationAmount
+        val savedValidationAmount = validationAmountRepository.saveAndFlush(validationAmount)
+
+        // Update the validationDate in the Account table
+        if (accountOptional.isPresent) {
+            val account = accountOptional.get()
+            account.validationDate = validationAmount.dateUpdated
+            account.dateUpdated = validationAmount.dateUpdated
+            accountRepository.saveAndFlush(account)
+        }
+
+        return savedValidationAmount
     }
 
     @Timed
