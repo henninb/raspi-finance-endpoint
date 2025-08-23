55ca9feeaff1812aea3a7b093fc1002835e41c71
Brian Henning <henninb@msn.com>
2025-08-23 05:56:29 -0500

    graphql logging

--- src/main/kotlin/finance/configurations/AliasWebConfig.kt
+++ src/main/kotlin/finance/configurations/AliasWebConfig.kt
@@ -1,12 +1,22 @@
 package finance.configurations
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 open class AliasWebConfig : WebMvcConfigurer {
+
+    @Autowired
+    lateinit var requestLoggingInterceptor: RequestLoggingInterceptor
+
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("/health").setViewName("forward:/actuator/health")
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(requestLoggingInterceptor)
     }
 }
