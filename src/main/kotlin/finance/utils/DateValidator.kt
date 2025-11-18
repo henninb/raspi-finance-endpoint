1e2ed60574a72179f5ecf1e8e6b95eb9064f4010
Brian Henning <henninb@msn.com>
2025-11-17 18:25:52 -0600

    wip

--- src/main/kotlin/finance/utils/DateValidator.kt
+++ src/main/kotlin/finance/utils/DateValidator.kt
@@ -3,18 +3,18 @@ package finance.utils
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 import org.apache.logging.log4j.LogManager
-import java.sql.Date
+import java.time.LocalDate
 
-class DateValidator : ConstraintValidator<ValidDate, Date?> {
+class DateValidator : ConstraintValidator<ValidDate, LocalDate?> {
     override fun initialize(constraintAnnotation: ValidDate) {
     }
 
     override fun isValid(
-        value: Date?,
+        value: LocalDate?,
         context: ConstraintValidatorContext,
     ): Boolean {
         logger.debug("dateToBeEvaluated: $value")
-        return value == null || value > Date.valueOf("2000-01-01")
+        return value == null || value.isAfter(LocalDate.of(2000, 1, 1))
     }
 
     companion object {
