5aa5ef7538e90bbaaf07d9f76d743b4845da1132
Brian Henning <henninb@gmail.com>
2026-02-01 06:02:54 -0600

    wip

--- src/main/kotlin/finance/domain/Payment.kt
+++ src/main/kotlin/finance/domain/Payment.kt
@@ -71,11 +71,11 @@ data class Payment(
     @param:JsonProperty
     @field:Pattern(regexp = UUID_PATTERN, message = FIELD_MUST_BE_UUID_MESSAGE)
     @Column(name = "guid_source", nullable = false)
-    var guidSource: String,
+    var guidSource: String? = null,
     @param:JsonProperty
     @field:Pattern(regexp = UUID_PATTERN, message = FIELD_MUST_BE_UUID_MESSAGE)
     @Column(name = "guid_destination", nullable = false)
-    var guidDestination: String,
+    var guidDestination: String? = null,
     @param:JsonProperty
     @Column(name = "active_status", nullable = false, columnDefinition = "BOOLEAN DEFAULT TRUE")
     var activeStatus: Boolean = true,
