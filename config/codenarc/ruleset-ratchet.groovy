b1c0c10dc3a0f10811e13fbce2478b312df1fc1e
Brian Henning <henninb@gmail.com>
2026-05-23 19:07:15 -0500

    wip

--- config/codenarc/ruleset-ratchet.groovy
+++
@@ -1,8 +0,0 @@
-ruleset {
-    description 'Ratchet rules: only fail for a narrow set on main'
-
-    // Keep this intentionally tiny and high-signal
-    UnusedImport()
-    TrailingWhitespace()
-}
-
